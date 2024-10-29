6812961488d276811fe995b2648dea765332291e
NickShum <115332882+NickShum222@users.noreply.github.com>
2024-10-29 10:52:00 -0400

    Added Objective Card Prototypes

--- src/Components/ObjectiveCard/index.js
+++ src/Components/ObjectiveCard/index.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 const index = ({title, children}) => {
   return (
-    <div className='bg-card_background rounded-3xl h-[420px] sm:h-[520px] overflow-hidden relative'>
-    <div className="mt-10 sm:mt-14 text-center relative z-10">
+    <div className='bg-card_background rounded-3xl h-[420px] sm:h-[520px] overflow-hidden w-full relative'>
+    <div className="mt-10 sm:mt-14 text-center z-10">
       <h3 className="text-2xl tracking-[-0.02em] sm:text-3xl z-10">{title}</h3></div>
     <div className="flex-grow flex items-center justify-center mt-10 md:mt-14 scale-90 sm:scale-100">
-    </div>
     {children}
+    </div>
     </div>
   )
 }
--- src/sections/Objective.jsx
+++ src/sections/Objective.jsx
@@ -21,23 +21,69 @@ const Objective = () => {
               </div>
             </div>
           </ObjectiveCard>
-          <ObjectiveCard title="Get exclusive datapoints"></ObjectiveCard>
-          <ObjectiveCard title="Seamless integrations"></ObjectiveCard>
-          <ObjectiveCard title="Accurate employee vesting schedules"></ObjectiveCard>
-          <ObjectiveCard title="Company emails and contacts"></ObjectiveCard>
-          <ObjectiveCard title="Competitor Maps"></ObjectiveCard>
+          <ObjectiveCard title="Get exclusive datapoints">
+            <div className="items-center h-full w-full px-4">
+              <div className="grid grid-cols-2 gap-4 px-[10%]">
+                {[...Array(4)].map((_, i) => (
+                  <DataPointCard key={i} />
+                ))}
+              </div>
+            </div>
+          </ObjectiveCard>
+          <ObjectiveCard title="Seamless integrations">
+            <div className="flex flex-1 justify-center items-center gap-2 h-full w-full px-[15%] mt-[15%]">
+              {[...Array(5)].map((_, i) => (
+                <IntegrationBadge key={i} />
+              ))}
+            </div>
+          </ObjectiveCard>
+          <ObjectiveCard title="Accurate employee vesting schedules">
+          <div className="items-center h-full w-full px-4">
+              <div className="flex flex-col items-center justify-center gap-4 px-[10%] flex-1">
+                <div className="bg-black w-full aspect-video rounded-md"/>
+                <div className="rounded-full w-[50%] bg-aviato_green h-[45px]"/>
+              </div>
+            </div>
+          </ObjectiveCard>
+          <ObjectiveCard title="Company emails and contacts">
+          <div className="items-center h-full w-full px-4">
+              <div className="flex flex-col items-center justify-center gap-4 px-[15%] flex-1 relative">
+                <div className="bg-black w-full aspect-[28/9] rounded-md"/>
+                <div className="bg-black w-full aspect-[36/9] rounded-md"/>
+
+                <div className="rounded-full w-[50%] bg-aviato_green aspect-[42/9]"/>
+              </div>
+            </div>
+          </ObjectiveCard>
+          <ObjectiveCard title="Competitor Maps">
+          <div className="items-center h-full w-full px-4">
+              <div className="flex flex-col items-center justify-center gap-4 px-[12%] flex-1 relative">
+              <div className="rounded-full w-[50%] bg-aviato_green aspect-[42/9]"/>
+
+                <div className="bg-white grid grid-cols-3 rounded-[16px] w-full gap-2 aspect-[14/9] p-8">
+                {[...Array(6)].map((_, i) => (
+                <div className="bg-card_background w-full rounded-md"/>
+              ))}
+                </div>
+              </div>
+            </div>
+          </ObjectiveCard>
         </div>
       </section>
     </>
   );
 };
 
-const TopEngineerCard = (() => {
+const TopEngineerCard = () => {
   return (
-    <div className="bg-black rounded-md md:w-[126px] md:h-[126px] max-md:w-[18vw] max-md:h-[18vw]">
-
-    </div>
-  )
-}) 
+    <div className="bg-black rounded-md md:w-[126px] md:h-[126px] max-md:w-[18vw] max-md:h-[18vw]" />
+  );
+};
+const DataPointCard = () => {
+  return <div className="bg-black rounded-md w-full h-auto aspect-square" />;
+};
+const IntegrationBadge = () => {
+  return <div className="bg-black rounded-full w-full h-full aspect-square" />;
+};
 
 export default Objective;
