93004881d4914d5d199be5ac1d0ab7d51dea4af5
NickShum <115332882+NickShum222@users.noreply.github.com>
2024-10-29 11:28:39 -0400

    Removed Dynanmic Import

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import dynamic from "next/dynamic";
 import { Navbar, SEO } from "@/Components";
+import { Landing, About, Objective, Action, Footer } from "@/sections";
 
-const Landing = dynamic(() => import("@/sections/Landing"));
-const About = dynamic(() => import("@/sections/About"));
-const Objective = dynamic(() => import("@/sections/Objective"));
-const Action = dynamic(() => import("@/sections/Action"));
-const Footer = dynamic(() => import("@/sections/Footer"));
+// const Landing = dynamic(() => import("@/sections/Landing"));
+// const About = dynamic(() => import("@/sections/About"));
+// const Objective = dynamic(() => import("@/sections/Objective"));
+// const Action = dynamic(() => import("@/sections/Action"));
+// const Footer = dynamic(() => import("@/sections/Footer"));
 
 export default function Home() {
   return (
